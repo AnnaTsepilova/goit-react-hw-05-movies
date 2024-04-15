c7dddbc203127c498f1c764264d687f741f91972
Anna Tsepilova <virgo.tsav@gmail.com>
2024-04-15 16:10:59 +0300

    refresh src

---
+++ src/components/CastList/CastList.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const CastListContainer = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  gap: 30px;
+
+  margin: 0 auto;
+  padding: 0 15px 36px 15px;
+  max-width: 1440px;
+
+  list-style: none;
+`;
